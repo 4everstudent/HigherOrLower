98edc79f4cfa62cad59c05af43ec115418922611
4EverStudent <mail@4everstudent.com>
2016-09-04 20:28:00 +0100

    Add function guessNumber

--- app/src/main/java/a4everstudent/higherorlower/MainActivity.java
+++ app/src/main/java/a4everstudent/higherorlower/MainActivity.java
@@ -2,12 +2,50 @@ package a4everstudent.higherorlower;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    int generated_number;
+
+
+    public void guessNumber(View view){
+
+        EditText user_guess_text = (EditText) findViewById(R.id.user_guess);
+        int  user_guess = Integer.parseInt(user_guess_text.getText().toString());
+        String message = "";
+
+
+
+        if(user_guess == generated_number){
+            message = "You rock, that's correct!";
+        }
+        else{
+            if(user_guess < generated_number){
+                message = "Too low!";
+            }
+            else{
+                if(user_guess > generated_number){
+                    message = "Too high!";
+                }
+                else{
+                    message = "Something strange happened, please inform my developer, maybe you'll get a chocolat";
+                }
+
+            }
+
+        }
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Random randomGenerator = new Random();
+        generated_number = randomGenerator.nextInt(100)+1;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
